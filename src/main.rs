ba5e7cb7d31e2da5ac89188092ea2506f4c219b5
BigBoss9324 <ADJordan305@hotmail.com>
2025-09-19 02:46:35 -0500

    testing Rust

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,47 @@
-use std::io;
+use std::io::{self, Write};
+
+fn get_new_number(number: &mut i32) -> Result<(), &'static str> {
+    print!("Enter a new number: ");
+    io::stdout().flush().unwrap();
+    let mut new_number = String::new();
+    io::stdin().read_line(&mut new_number).map_err(|_| "Failed to read input")?;
+    let parsed: i32 = new_number.trim().parse().map_err(|_| "Invalid number")?;
+    if parsed < 0 {
+        return Err("Number must be positive");
+    }
+    *number = parsed;
+    Ok(())
+}
 
 fn main() {
-    println!("Guess the number!");
+    println!("The current directory is: {:?}", std::env::current_dir().unwrap());
 
-    println!("Please input your guess.");
+    let mut number = 42;
 
-    let mut guess = String::new();
+    println!("Number is {}", number);
+    println!("=== Would you like to change it? ===");
+    println!("(y/n)");
 
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Failed to read line");
+    let mut input = String::new();
+    std::io::stdin().read_line(&mut input).unwrap();
 
-    println!("You guessed: {guess}");
+    if input.trim() == "y" {
+        loop {
+            if let Err(e) = get_new_number(&mut number) {
+                println!("Error getting new number: {}", e);
+            } else {
+                println!("Number changed to {}", number);
+                break;
+            }
+        }
+
+
+
+    } else {
+        println!("Number remains {}", number);
+    }
+
+    println!("Number is {}", number);
+
+    println!("=== End of Program ===");
 }
